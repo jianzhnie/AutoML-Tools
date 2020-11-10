b5358ccb1f016adc990cbb601548a8abff607123
jianzhnie <jianzhnie@126.com>
2020-11-10 16:11:55 +0800

    add hyperopt examples

--- examples/hyperopt_rf.py
+++ examples/hyperopt_rf.py
@@ -11,4 +11,45 @@ import warnings
 warnings.filterwarnings("ignore")
 
 
-data = pd.read_csv("../data/mobile_price_data.csv")
+data = pd.read_csv("../data/mobile_price_data/train.csv")
+
+# load data
+X = data.drop("price_range", axis=1).values 
+y = data.price_range.values
+
+# 标准化特征变量
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
+
+# 定义搜索空间
+space = {
+    "n_estimators": hp.choice("n_estimators", [100, 200, 300, 400, 500, 600]),
+    "max_depth": hp.quniform("max_depth", 1, 15, 1),
+    "criterion": hp.choice("criterion", ["gini", "entropy"]),
+}
+
+
+# 定义目标函数
+def hyperparameter_tuning(params):
+    clf = RandomForestClassifier(**params, n_jobs=-1)
+    acc = cross_val_score(clf, X_scaled, y, scoring="accuracy").mean()
+    return {"loss": -acc, "status": STATUS_OK}
+
+
+# 初始化Trial 对象
+trials = Trials()
+best = fmin(
+    fn=hyperparameter_tuning,
+    space=space,
+    algo=tpe.suggest,
+    max_evals=100,
+    trials=trials
+)
+
+if __name__ == "__main__":
+    print("Best: {}".format(best))
+    print(trials.results)
+    print(trials.losses())
+    print(trials.statuses())
+
+
